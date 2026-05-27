e6636906f323bc98991811fa8424ba527432eba0
cachar666 <cachar@gmail.com>
2026-05-27 01:44:23 -0500

    Album Mundial 2026 - PWA

---
+++ sw.js
@@ -0,0 +1,18 @@
+var CACHE='mundial2026-v1';
+var URLS=['./', './index.html', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(URLS);}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(caches.keys().then(function(names){
+    return Promise.all(names.filter(function(n){return n!==CACHE;}).map(function(n){return caches.delete(n);}));
+  }));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e){
+  e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request);}));
+});
