6cf7783400f5a9cdb5ffac604b891c2f22b89ef4
cachar666 <cachar@gmail.com>
2026-05-29 13:06:44 -0500

    Optimizacion PWA: preconnect, debounce, stale-while-revalidate, manifest mejorado + SW v31

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='mundial2026-v30';
+var CACHE='mundial2026-v31';
 var URLS=['./', './index.html', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(e){
@@ -15,16 +15,28 @@ self.addEventListener('activate', function(e){
 
 self.addEventListener('fetch', function(e){
   var url=e.request.url;
+  // Flag images: cache-first
   if(url.indexOf('flagcdn.com')>=0){
     e.respondWith(caches.match(e.request).then(function(r){
       if(r) return r;
       return fetch(e.request).then(function(resp){
-        var clone=resp.clone();
-        caches.open(CACHE).then(function(c){c.put(e.request,clone);});
+        if(resp.ok){var clone=resp.clone();caches.open(CACHE).then(function(c){c.put(e.request,clone);});}
         return resp;
-      });
+      }).catch(function(){return new Response('',{status:404});});
     }));
-  } else {
+  }
+  // HTML: stale-while-revalidate (serve cache, update in background)
+  else if(url.indexOf('.html')>=0||url.endsWith('/')){
+    e.respondWith(caches.match(e.request).then(function(cached){
+      var fetchPromise=fetch(e.request).then(function(resp){
+        if(resp.ok){var clone=resp.clone();caches.open(CACHE).then(function(c){c.put(e.request,clone);});}
+        return resp;
+      }).catch(function(){return cached;});
+      return cached||fetchPromise;
+    }));
+  }
+  // Everything else: cache-first
+  else {
     e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request);}));
   }
 });
